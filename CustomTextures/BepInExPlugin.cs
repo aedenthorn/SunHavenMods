10f3d40db3cec6d307f95cc03f33db2bc69d96ca
aedenthorn <aedenthorn1@gmail.com>
2021-10-01 22:07:00 -0400

    etc

---
+++ CustomTextures/BepInExPlugin.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Wish;
+
+namespace CustomTextures
+{
+    [BepInPlugin("aedenthorn.CustomTextures", "Custom Textures", "0.1.0")]
+    public partial class BepInExPlugin : BaseUnityPlugin
+    {
+        private static BepInExPlugin context;
+
+        public static ConfigEntry<bool> modEnabled;
+        public static ConfigEntry<bool> isDebug;
+        //public static ConfigEntry<int> nexusID;
+
+        public static Dictionary<string, Texture2D> customTextureDict;
+        public static void Dbgl(string str = "", bool pref = true)
+        {
+            if (isDebug.Value)
+                Debug.Log((pref ? typeof(BepInExPlugin).Namespace + " " : "") + str);
+        }
+        private void Awake()
+        {
+
+            context = this;
+            modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
+            isDebug = Config.Bind<bool>("General", "IsDebug", true, "Enable debug logs");
+            //nexusID = Config.Bind<int>("General", "NexusID", 1, "Nexus mod ID for updates");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+
+            LoadCustomTextures();
+        }
+
+        private void LoadCustomTextures()
+        {
+            customTextureDict = new Dictionary<string, Texture2D>();
+            string path = AedenthornUtils.GetAssetPath(this, true);
+            foreach(string file in Directory.GetFiles(path, "*.png", SearchOption.AllDirectories))
+            {
+                Texture2D tex = new Texture2D(1, 1);
+                tex.LoadImage(File.ReadAllBytes(file));
+                customTextureDict.Add(Path.GetFileNameWithoutExtension(file), tex);
+            }
+            Dbgl($"Loaded {customTextureDict.Count} textures");
+        }
+
+        [HarmonyPatch(typeof(CharacterClothingStyles), "SetupClothingStyleDictionary")]
+        static class ClothingLayerData_ClotherLayerInfo_Patch
+        {
+            static void Prefix()
+            {
+                foreach (ClothingLayerData cld in CharacterClothingStyles.allClothing)
+                {
+                    foreach (ClothingLayerInfo cli in cld.ClotherLayerInfo)
+                    {
+                        for(int i = 0; i < cli.sprites.Length; i++)
+                        {
+                            if (cli.sprites[i] && customTextureDict.ContainsKey(Regex.Replace(cli.sprites[i].name, "_[0-9]+$","")))
+                            {
+                                Dbgl($"replacing sprite {cli.sprites[i].name}");
+                                //File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), typeof(BepInExPlugin).Namespace, cli.sprites[i].name+".png"), cli.sprites[i].texture.EncodeToPNG());
+                                cli.sprites[i] = Sprite.Create(customTextureDict[Regex.Replace(cli.sprites[i].name, "_[0-9]+$", "")], cli.sprites[i].rect, new Vector2(cli.sprites[i].pivot.x / cli.sprites[i].rect.width, cli.sprites[i].pivot.y / cli.sprites[i].rect.height), cli.sprites[i].pixelsPerUnit);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
